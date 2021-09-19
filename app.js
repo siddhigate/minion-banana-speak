eb6bb3f5feb5d2cbc8752cc4fcc4aa646c996057
DESKTOP-FJAKAS7\Siddhi <siddhigate16@gmail.com>
2021-09-19 09:21:54 +0530

    fix: empty input and reset error output issue

--- app.js
+++ app.js
@@ -3,32 +3,39 @@ var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 var errorDiv = document.querySelector("#errorMessage");
 
-var serverUrl= "https://api.funtranslations.com/translate/minion.json";
+var serverUrl = "https://api.funtranslations.com/translate/minion.json";
 
 // formatting url
-function getTranslationUrl(text){
-    return serverUrl +"?"+"text="+text;
+function getTranslationUrl(text) {
+    return serverUrl + "?" + "text=" + text;
 }
 
 // error handling function
-function errorHandler(error){
-    errorDiv.innerText="We are sorry. You can only use this for 5 times per hour. Try again later.";
+function errorHandler(error) {
+    errorDiv.innerText = "We are sorry. You can only use this for 5 times per hour. Try again later.";
 }
 
 // click event
-btnTranslate.addEventListener("click", clickEventHandler())
+btnTranslate.addEventListener("click", () => {
+
+    errorDiv.innerText = "";
+    outputDiv.innerText = "";
+    clickEventHandler();
+})
 
 // handling click event 
 function clickEventHandler() {
-    return function clickEventHandler() {
-        var inputText = txtInput.value;
-
+    
+    var inputText = txtInput.value;
+    if (inputText === "") {
+        errorDiv.innerText = "Please enter some text";
+    } else {
         fetch(getTranslationUrl(inputText))
-        .then(response=>response.json())
-        .then(json => outputDiv.innerText=json.contents.translated)
-        .catch(errorHandler)
-        ;
+            .then(response => response.json())
+            .then(json => outputDiv.innerText = json.contents.translated)
+            .catch(errorHandler)
+            ;
+    }
 
-    };
 }
 
